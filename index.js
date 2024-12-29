47e5ab1accef2075929140a6d4f5c472f984c6c7
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-30 02:07:47 +0600

    revies api create

--- index.js
+++ index.js
@@ -30,11 +30,16 @@ async function run() {
     await client.connect();
 
 	const menuCollection = client.db('bistroDB').collection('menu');
+	const reviewsCollection = client.db('bistroDB').collection('reviews');
 
 
 	//all data load
 	app.get('/menu', async(req, res) =>{
 		const result = await menuCollection.find().toArray();
+		res.send(result);
+	})
+	app.get('/reviews', async(req, res) =>{
+		const result = await reviewsCollection.find().toArray();
 		res.send(result);
 	})
 
